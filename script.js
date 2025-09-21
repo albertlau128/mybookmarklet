723cfbb3746deb06d5c8bc1abf226fc88def259b
Albert LAU <32214383+albertlau128@users.noreply.github.com>
2025-09-22 00:13:36 +0800

    add metar/taf data to popup

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function fetchWeatherData() {
                         continue;
                     }
                     const row = document.createElement("tr");
-                    row.innerHTML = `<td>${key}</td>`;
+                    row.innerHTML = `<td><b>${key}</b></td><hr width="10px" color="red" size="5">`;
                     const row2 = document.createElement("tr");
                     row2.innerHTML = `<td>${value}</td>`;
                     tableBody.appendChild(row);
@@ -30,7 +30,24 @@ function fetchWeatherData() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    fetchMetarTafData();
     fetchWeatherData();
     setInterval(fetchWeatherData, 60 * 1000);
+    setInterval(fetchMetarTafData, 60 * 1000);
     document.getElementById("lang-select").addEventListener("change", fetchWeatherData);
 });
+
+function fetchMetarTafData(){
+    fetch(`https://aviationweather.gov/api/data/metar?ids=VHHH&hours=0&order=id%2C-obs&sep=true&taf=true`)
+        .then(response => response.text())
+        .then(data => {
+           splitedData=data.split('\n')
+            const metarCode = document.getElementById("metar-code");
+            const tafCode = document.getElementById("taf-code");
+            console.log(splitedData)
+            console.log(data)
+            metarCode.innerText = splitedData[0];
+            tafCode.innerText = splitedData[2];
+        })
+        .catch(error => console.error(error));
+}
